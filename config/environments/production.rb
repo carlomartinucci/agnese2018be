a5fa674d560055ce83e5b8e62adbe1983f48ea8f
ch <carlo.martinucci@gmail.com>
2018-08-14 22:57:53 +0200

    remove mailgun, add gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'agnese2018be.herokuapp.com',
-    :authentication => :plain,
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'carlo.martinucci@gmail.com',
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       :plain,
+    enable_starttls_auto: true
   }
 
   config.action_mailer.default_url_options = {
