10bc77961fc0458831487e4e37eef3e71ad588e3
ch <carlo.martinucci@gmail.com>
2018-08-15 09:42:56 +0200

    try to disable cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ end
 Rails.application.config.middleware.use ExceptionNotification::Rack,
   :email => {
     :deliver_with => :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
-    :email_prefix => "[ERROR] ",
+    :email_prefix => "[ERROR agnese2018] ",
     :sender_address => %{"notifier" <notifier@agnese2018.com>},
     :exception_recipients => %w{carlo.martinucci@gmail.com}
   }
